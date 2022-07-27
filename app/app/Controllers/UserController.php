536b9162b0e42ccf6ed51f1c845b75457fa9566e
gogeruk <gogeruk_locker@protonmail.com>
2022-07-28 00:32:50 +0300

    move validation

--- app/app/Controllers/UserController.php
+++ app/app/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Models\User;
 use App\Modules\View;
-use App\Modules\Validator;
+use App\Modules\ValidateForm;
 
 
 class UserController
@@ -28,7 +28,8 @@ class UserController
                 $role = 3; // worker
             }
 
-            $errors = $this->validateForm($email, $password, $errors);
+            $validateForm = new ValidateForm();
+            $errors = $validateForm->validateForm($email, $password, $errors);
             if (empty($errors['email']) && empty($errors['password'])) {
 
                 // save to db
@@ -37,7 +38,6 @@ class UserController
                 return View::display('Home', ['page' => 'HOME']);
             }
         }
-
         return View::display('New', ['page' => 'NEW', 'errors' => $errors]);
     }
 
@@ -53,7 +53,8 @@ class UserController
             $email = $_POST['email'];
             $password = $_POST['password'];
 
-            $errors = $this->validateForm($email, $password, $errors);
+            $validateForm = new ValidateForm();
+            $errors = $validateForm->validateForm($email, $password, $errors);
             if (empty($errors['email']) && empty($errors['password'])) {
 
                 // check if user exists
@@ -62,71 +63,6 @@ class UserController
                 return View::display('Home', ['page' => 'HOME']);
             }
         }
-
         return View::display('Login', ['page' => 'LOGIN', 'errors' => $errors]);
-    }
-
-
-
-    /**
-     * @param Validator $validator
-     * @param mixed $email
-     * @return array
-     */
-    public function validateEmail(Validator $validator, mixed $email): array
-    {
-        $errors[] = $validator->stringConstraint(
-            $email,
-            'Email must be valid'
-        );
-
-        $emailBits = explode('@', $email);
-        $errors[] = $validator->stringMinLengthConstraint(
-            $emailBits[0],
-            3,
-            'Email string before @ must be min 3 chars long'
-        );
-        $errors[] = $validator->stringExactLengthConstraint(
-            $emailBits[1] ?? '',
-            3,
-            'Email string after @ must be exactly 3 chars long'
-        );
-        return $errors;
-    }
-
-    /**
-     * @param Validator $validator
-     * @param $inputString
-     * @return array
-     */
-    public function validatePassword(Validator $validator, $inputString): array
-    {
-        $errors[] = $validator->stringMinLengthConstraint(
-            $inputString,
-            6,
-            'Password string must be min 6 chars long'
-        );
-        return $errors;
-    }
-
-    /**
-     * @param mixed $email
-     * @param mixed $password
-     * @param array $errors
-     * @return array
-     */
-    public function validateForm(mixed $email, mixed $password, array $errors): array
-    {
-        $validator = new Validator();
-        $errors['email'] = $this->validateEmail($validator, $email);
-        $errors['password'] = $this->validatePassword($validator, $password);
-
-        $errors['password'] = array_filter($errors['password'], function ($a) {
-            return $a !== null;
-        });
-        $errors['email'] = array_filter($errors['email'], function ($a) {
-            return $a !== null;
-        });
-        return $errors;
     }
 }
---
+++ app/app/Modules/ValidateForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Modules;
+
+class ValidateForm
+{
+    /**
+     * @param Validator $validator
+     * @param mixed $email
+     * @return array
+     */
+    public function validateEmail(Validator $validator, mixed $email): array
+    {
+        $errors[] = $validator->stringConstraint(
+            $email,
+            'Email must be valid'
+        );
+
+        $emailBits = explode('@', $email);
+        $errors[] = $validator->stringMinLengthConstraint(
+            $emailBits[0],
+            3,
+            'Email string before @ must be min 3 chars long'
+        );
+        $errors[] = $validator->stringExactLengthConstraint(
+            $emailBits[1] ?? '',
+            3,
+            'Email string after @ must be exactly 3 chars long'
+        );
+        return $errors;
+    }
+
+    /**
+     * @param Validator $validator
+     * @param $inputString
+     * @return array
+     */
+    public function validatePassword(Validator $validator, $inputString): array
+    {
+        $errors[] = $validator->stringMinLengthConstraint(
+            $inputString,
+            6,
+            'Password string must be min 6 chars long'
+        );
+        return $errors;
+    }
+
+    /**
+     * @param mixed $email
+     * @param mixed $password
+     * @param array $errors
+     * @return array
+     */
+    public function validateForm(mixed $email, mixed $password, array $errors): array
+    {
+        $validator = new Validator();
+        $errors['email'] = $this->validateEmail($validator, $email);
+        $errors['password'] = $this->validatePassword($validator, $password);
+
+        $errors['password'] = array_filter($errors['password'], function ($a) {
+            return $a !== null;
+        });
+        $errors['email'] = array_filter($errors['email'], function ($a) {
+            return $a !== null;
+        });
+        return $errors;
+    }
+}
