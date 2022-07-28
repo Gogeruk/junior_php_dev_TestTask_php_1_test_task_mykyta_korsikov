ff48d3ebd36f4ef4e85892e58fb2c0d7ba066a9c
gogeruk <gogeruk_locker@protonmail.com>
2022-07-28 18:30:11 +0300

    make 10 api calls

--- app/app/Views/MainData.php
+++ app/app/Views/MainData.php
@@ -3,10 +3,52 @@
 
 
 
-<h1>CALL API</h1>
-<h1>GET DATA</h1>
-<h1>DISPLAY UP TO 10</h1>
-<h1>WHEN BUTTON CLICKED --> SAVE TO DB</h1>
+<button class="m-3 btn btn-primary" type="button" onclick="javascript:getJson()">Get Json</button>
+
+<div class="mb-4">
+    <div class="m-3" id="newJsonCreate">
+    </div>
+
+</div>
+
+
+
+
+<script type="text/javascript" language="javascript">
+    let stop = false;
+    let clicked = 1;
+    const getJson = async () => {
+        if (stop === true) {
+            console.log(stop);
+            return;
+        }
+
+        const response = await fetch('http://jsonplaceholder.typicode.com/posts/' + clicked);
+        const myJson = await response.json();
+
+        clicked++;
+        if (clicked === 11) {
+            stop = true;
+        }
+
+        // do something with myJson
+        // console.log(myJson);
+
+
+        // var allReceived = [];
+
+
+        // create new element
+        const para = document.createElement("p");
+        const node = document.createTextNode(myJson.title);
+        para.appendChild(node);
+
+        const element = document.getElementById("newJsonCreate");
+        element.append(para);
+    }
+</script>
+
+
 
 
 
